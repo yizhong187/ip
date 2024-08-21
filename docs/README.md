cadf34d36093333d412fbdddf39216e35b3f4902
yizhong187 <yizhong187@gmail.com>
2024-08-22 03:44:23 +0800

    Renamed chatbot to Casper

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Casper User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Casper.java
@@ -0,0 +1,12 @@
+public class Casper {
+    public static void main(String[] args) {
+        System.out.println(
+                "____________________________________________________________ \n" +
+                        "Hello! I'm Casper \n" +
+                        "What can I do for you? \n" +
+                        "____________________________________________________________ \n" +
+                        "Bye. Hope to see you again soon! \n" +
+                        "____________________________________________________________"
+        );
+    }
+}
