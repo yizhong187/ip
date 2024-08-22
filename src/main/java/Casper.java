d8ea8f8450e7ddb80b0e5966fb7f6e49b25b717d
yizhong187 <yizhong187@gmail.com>
2024-08-22 11:18:01 +0800

    added functionality of storing tasks and returning

--- src/main/java/Casper.java
+++ src/main/java/Casper.java
@@ -3,6 +3,11 @@ import java.util.Scanner;
 
 public class Casper {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean chat = true;
+        String[] tasks = new String[100];
+        int currTaskIndex = 0;
+
         print("""
                 ____________________________________________________________\s
                 Hello! I'm Casper\s
@@ -11,25 +16,29 @@ public class Casper {
                 """
         );
 
-        Scanner scanner = new Scanner(System.in);
-
-        boolean chat = true;
-
         while (chat) {
             String input = scanner.nextLine();
+            print("____________________________________________________________");
 
-            if (!Objects.equals(input, "bye" )) {
-                print("____________________________________________________________ \n" +
-                        input + "\n" +
-                        "____________________________________________________________ \n" );
-            } else {
+            if (Objects.equals(input, "bye" )) {
                 chat = false;
-                print("""
-                        ____________________________________________________________\s
-                        Bye. Hope to see you again soon!\s
-                        ____________________________________________________________\s
-                        """);
+                print("Bye. Hope to see you again soon!");
+
+            } else if (Objects.equals(input, "list" )){
+                for (int i = 0; i < tasks.length; i++) {
+                    if (tasks[i] == null) {
+                        break;
+                    }
+                    print(i + ". " + tasks[i]);
+                }
+
+            } else {
+                tasks[currTaskIndex] = input;
+                currTaskIndex++;
+                print("added: " + input);
             }
+
+            print("____________________________________________________________\n");
         }
     }
 
