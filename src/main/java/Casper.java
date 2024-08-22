13ccb288fb4320f41e2244f561a1ef9e3b7fcd39
yizhong187 <yizhong187@gmail.com>
2024-08-22 10:51:59 +0800

    added functionality of echoing commands

--- src/main/java/Casper.java
+++ src/main/java/Casper.java
@@ -1,12 +1,39 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Casper {
     public static void main(String[] args) {
-        System.out.println(
-                "____________________________________________________________ \n" +
-                        "Hello! I'm Casper \n" +
-                        "What can I do for you? \n" +
-                        "____________________________________________________________ \n" +
-                        "Bye. Hope to see you again soon! \n" +
-                        "____________________________________________________________"
+        print("""
+                ____________________________________________________________\s
+                Hello! I'm Casper\s
+                What can I do for you?\s
+                ____________________________________________________________\s
+                """
         );
+
+        Scanner scanner = new Scanner(System.in);
+
+        boolean chat = true;
+
+        while (chat) {
+            String input = scanner.nextLine();
+
+            if (!Objects.equals(input, "bye" )) {
+                print("____________________________________________________________ \n" +
+                        input + "\n" +
+                        "____________________________________________________________ \n" );
+            } else {
+                chat = false;
+                print("""
+                        ____________________________________________________________\s
+                        Bye. Hope to see you again soon!\s
+                        ____________________________________________________________\s
+                        """);
+            }
+        }
+    }
+
+    private static void print(String text) {
+        System.out.println(text);
     }
 }
