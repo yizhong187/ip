8ad46f5f56ec194c04986c839ab3268e075d78bf
yizhong187 <yizhong187@gmail.com>
2024-08-29 15:28:04 +0800

    Initial commit

--- src/main/java/exceptions/CorruptedSavedTasksException.java
+++ src/main/java/exceptions/CorruptedSavedTasksException.java
@@ -1,2 +1,8 @@
-package exceptions;public class CorruptedSavedTasksException {
+package exceptions;
+
+public class CorruptedSavedTasksException extends ExceptionWithSolution{
+    public CorruptedSavedTasksException(int lineNumber) {
+        super(String.format("Line %d in locally saved tasks list is corrupted", lineNumber),
+                "You may want to edit `casper.txt` manually, or delete the file to reset.");
+    }
 }
--- src/main/java/exceptions/InvalidDateTimeException.java
+++ src/main/java/exceptions/InvalidDateTimeException.java
@@ -1,2 +1,8 @@
-package exceptions;public class InvalidDateTimeException {
+package exceptions;
+
+public class InvalidDateTimeException extends ExceptionWithSolution{
+    public InvalidDateTimeException() {
+        super("Invalid date-time format.",
+                "Please provide the date-time as 'dd-MM-yyyy HH:mm'. (E.g., 29-08-2024 15:30)");
+    }
 }
