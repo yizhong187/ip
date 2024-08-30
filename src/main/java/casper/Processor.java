b4724629be883a5b0c0b4302cb777939b0604851
yizhong187 <yizhong187@gmail.com>
2024-08-30 14:09:09 +0800

    Move CommandProcessor into casper package and renamed to Processor

---
+++ src/main/java/casper/Processor.java
@@ -0,0 +1,103 @@
+package casper;
+
+import casper.exceptions.ExceptionWithSolution;
+import casper.exceptions.CustomIOException;
+import casper.exceptions.MissingArgumentException;
+import casper.exceptions.InvalidCommandException;
+import casper.tasks.Deadline;
+import casper.tasks.Event;
+import casper.tasks.ToDo;
+
+import java.util.Objects;
+
+import static casper.utils.DateTimeUtils.convertStringToDateTime;
+
+public class Processor {
+    public static void processInput(String input, TaskList taskList) throws
+            ExceptionWithSolution, CustomIOException {
+
+        String[] inputParts = Parser.parseInputToCommandAndArgument(input);
+        String command = inputParts[0];
+        String argument = inputParts.length > 1 ? inputParts[1] : null;
+
+        switch (command) {
+
+            case "list" -> {
+                Ui.print(taskList.toString());
+            }
+
+            case "mark", "unmark", "delete" -> {
+                Parser.argumentCheck(argument, command);
+                taskList.checkIndexValidity(argument, command);
+
+                int taskIndex = Integer.parseInt(argument) - 1;
+
+                if (Objects.equals(command, "mark")) {
+                    taskList.markTask(taskIndex);
+                    Storage.editTaskInSavedTasks(CasperChat.FILE_PATH,
+                            taskList.getTaskSaveString(taskIndex),
+                            taskIndex + 1);
+                    Ui.print("Nice! I've marked this task as done:\n" + taskList.getTaskString(taskIndex));
+
+                } else if (Objects.equals(command, "unmark")){
+                    taskList.unmarkTask(taskIndex);
+                    Storage.editTaskInSavedTasks(CasperChat.FILE_PATH,
+                            taskList.getTaskSaveString(taskIndex),
+                            taskIndex + 1);
+                    Ui.print("I have unmarked this task:\n" + taskList.getTaskString(taskIndex));
+
+                } else {
+                    String toBeDeletedTaskString = taskList.getTaskString(taskIndex);
+                    taskList.removeTask(taskIndex);
+                    Storage.removeTaskFromSavedTasks(CasperChat.FILE_PATH, taskIndex + 1);
+                    Ui.print("I have deleted the task:\n" + toBeDeletedTaskString);
+                }
+            }
+
+            case "todo", "deadline", "event" -> {
+                Parser.argumentCheck(argument, command);
+
+                if (Objects.equals(command, "todo")) {
+                    taskList.addTask(new ToDo(argument));
+
+                } else if (Objects.equals(command, "deadline")){
+                    String[] parsedForBy = Parser.parseArgumentByKeyword(argument, "by");
+                    String description = parsedForBy[0];
+                    if (parsedForBy.length == 1) {
+                        throw new MissingArgumentException("deadline", "/by",
+                                "deadline [task description] /by [deadline]");
+                    }
+                    String by = parsedForBy[1];
+
+                    taskList.addTask(new Deadline(description, convertStringToDateTime(by)));
+
+                } else {
+                    String[] parsedForFrom = Parser.parseArgumentByKeyword(argument, "from");
+                    String description = parsedForFrom[0];
+                    if (parsedForFrom.length == 1) {
+                        throw new MissingArgumentException("event", "/from",
+                                "event [task description] /from [start time] /to [end time]");
+                    }
+
+                    String[] parsedForTo = Parser.parseArgumentByKeyword(parsedForFrom[1], "to");
+                    String from = parsedForTo[0];
+                    if (parsedForTo.length == 1) {
+                        throw new MissingArgumentException("event", "/to",
+                                "event [task description] /from [start time] /to [end time]");
+                    }
+                    String to = parsedForTo[1];
+
+                    taskList.addTask(new Event(description,
+                            convertStringToDateTime(from),
+                            convertStringToDateTime(to)));
+                }
+
+                Storage.addTaskToSavedTasks(CasperChat.FILE_PATH, taskList.getTaskSaveString(taskList.size() - 1));
+                Ui.printAddedTaskMessage(taskList.getTaskString(taskList.size() - 1), taskList.size());
+            }
+
+            default ->
+                    throw new InvalidCommandException(command);
+        }
+    }
+}
