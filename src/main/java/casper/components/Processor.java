6cdb46d9c90d4ed2ac6e5e740a998d8427f2ba38
yizhong187 <yizhong187@gmail.com>
2024-09-05 18:04:38 +0800

    Update methods to return string rather than print

--- src/main/java/casper/components/Processor.java
+++ src/main/java/casper/components/Processor.java
@@ -1,6 +1,6 @@
 package casper.components;
 
-import casper.CasperChat;
+import casper.Casper;
 import casper.exceptions.ExceptionWithSolution;
 import casper.exceptions.CustomIOException;
 import casper.exceptions.MissingArgumentException;
@@ -28,7 +28,7 @@ public class Processor {
      * @throws MissingArgumentException If required arguments are missing for a specific command.
      * @throws InvalidCommandException If the command is invalid or not recognized.
      */
-    public static void processInput(String input, TaskList taskList) throws
+    public static String processInput(String input, TaskList taskList) throws
             ExceptionWithSolution, CustomIOException {
 
         String[] inputParts = Parser.parseInputToCommandAndArgument(input);
@@ -36,87 +36,90 @@ public class Processor {
         String argument = inputParts.length > 1 ? inputParts[1] : null;
 
         switch (command) {
-            case "list" -> {
-                Ui.print(taskList.toString());
+        case "bye" -> {
+            return "Bye! You can close this window now or continue chatting anytime you want.";
+        }
+        case "list" -> {
+            return taskList.toString();
+        }
+
+        case "mark", "unmark", "delete" -> {
+            Parser.argumentCheck(argument, command);
+            taskList.checkIndexValidity(argument, command);
+
+            int taskIndex = Integer.parseInt(argument) - 1;
+
+            if (Objects.equals(command, "mark")) {
+                taskList.markTask(taskIndex);
+                Storage.editTaskInSavedTasks(Casper.FILE_PATH,
+                        taskList.getTaskSaveString(taskIndex),
+                        taskIndex + 1);
+                return "Nice! I've marked this task as done:\n" + taskList.getTaskString(taskIndex);
+
+            } else if (Objects.equals(command, "unmark")) {
+                taskList.unmarkTask(taskIndex);
+                Storage.editTaskInSavedTasks(Casper.FILE_PATH,
+                        taskList.getTaskSaveString(taskIndex),
+                        taskIndex + 1);
+                return "I have unmarked this task:\n" + taskList.getTaskString(taskIndex);
+
+            } else {
+                String toBeDeletedTaskString = taskList.getTaskString(taskIndex);
+                taskList.removeTask(taskIndex);
+                Storage.removeTaskFromSavedTasks(Casper.FILE_PATH, taskIndex + 1);
+                return "I have deleted the task:\n" + toBeDeletedTaskString;
             }
+        }
 
-            case "mark", "unmark", "delete" -> {
-                Parser.argumentCheck(argument, command);
-                taskList.checkIndexValidity(argument, command);
+        case "find" -> {
+            Parser.argumentCheck(argument, command);
+            return Stringifier.getFoundTasks(taskList.find(argument), argument);
+        }
 
-                int taskIndex = Integer.parseInt(argument) - 1;
+        case "todo", "deadline", "event" -> {
+            Parser.argumentCheck(argument, command);
 
-                if (Objects.equals(command, "mark")) {
-                    taskList.markTask(taskIndex);
-                    Storage.editTaskInSavedTasks(CasperChat.FILE_PATH,
-                            taskList.getTaskSaveString(taskIndex),
-                            taskIndex + 1);
-                    Ui.print("Nice! I've marked this task as done:\n" + taskList.getTaskString(taskIndex));
+            if (Objects.equals(command, "todo")) {
+                taskList.addTask(new ToDo(argument));
 
-                } else if (Objects.equals(command, "unmark")){
-                    taskList.unmarkTask(taskIndex);
-                    Storage.editTaskInSavedTasks(CasperChat.FILE_PATH,
-                            taskList.getTaskSaveString(taskIndex),
-                            taskIndex + 1);
-                    Ui.print("I have unmarked this task:\n" + taskList.getTaskString(taskIndex));
-
-                } else {
-                    String toBeDeletedTaskString = taskList.getTaskString(taskIndex);
-                    taskList.removeTask(taskIndex);
-                    Storage.removeTaskFromSavedTasks(CasperChat.FILE_PATH, taskIndex + 1);
-                    Ui.print("I have deleted the task:\n" + toBeDeletedTaskString);
+            } else if (Objects.equals(command, "deadline")) {
+                String[] parsedForBy = Parser.parseArgumentByKeyword(argument, "by");
+                String description = parsedForBy[0];
+                if (parsedForBy.length == 1) {
+                    throw new MissingArgumentException("deadline", "/by",
+                            "deadline [task description] /by [deadline]");
                 }
-            }
+                String by = parsedForBy[1];
 
-            case "find" -> {
-                Parser.argumentCheck(argument, command);
-                Ui.printFoundTasks(taskList.find(argument), argument);
-            }
+                taskList.addTask(new Deadline(description, convertStringToDateTime(by)));
 
-            case "todo", "deadline", "event" -> {
-                Parser.argumentCheck(argument, command);
-
-                if (Objects.equals(command, "todo")) {
-                    taskList.addTask(new ToDo(argument));
-
-                } else if (Objects.equals(command, "deadline")){
-                    String[] parsedForBy = Parser.parseArgumentByKeyword(argument, "by");
-                    String description = parsedForBy[0];
-                    if (parsedForBy.length == 1) {
-                        throw new MissingArgumentException("deadline", "/by",
-                                "deadline [task description] /by [deadline]");
-                    }
-                    String by = parsedForBy[1];
-
-                    taskList.addTask(new Deadline(description, convertStringToDateTime(by)));
-
-                } else {
-                    String[] parsedForFrom = Parser.parseArgumentByKeyword(argument, "from");
-                    String description = parsedForFrom[0];
-                    if (parsedForFrom.length == 1) {
-                        throw new MissingArgumentException("event", "/from",
-                                "event [task description] /from [start time] /to [end time]");
-                    }
-
-                    String[] parsedForTo = Parser.parseArgumentByKeyword(parsedForFrom[1], "to");
-                    String from = parsedForTo[0];
-                    if (parsedForTo.length == 1) {
-                        throw new MissingArgumentException("event", "/to",
-                                "event [task description] /from [start time] /to [end time]");
-                    }
-                    String to = parsedForTo[1];
-
-                    taskList.addTask(new Event(description,
-                            convertStringToDateTime(from),
-                            convertStringToDateTime(to)));
+            } else {
+                String[] parsedForFrom = Parser.parseArgumentByKeyword(argument, "from");
+                String description = parsedForFrom[0];
+                if (parsedForFrom.length == 1) {
+                    throw new MissingArgumentException("event", "/from",
+                            "event [task description] /from [start time] /to [end time]");
                 }
 
-                Storage.addTaskToSavedTasks(CasperChat.FILE_PATH, taskList.getTaskSaveString(taskList.size() - 1));
-                Ui.printAddedTaskMessage(taskList.getTaskString(taskList.size() - 1), taskList.size());
+                String[] parsedForTo = Parser.parseArgumentByKeyword(parsedForFrom[1], "to");
+                String from = parsedForTo[0];
+                if (parsedForTo.length == 1) {
+                    throw new MissingArgumentException("event", "/to",
+                            "event [task description] /from [start time] /to [end time]");
+                }
+                String to = parsedForTo[1];
+
+                taskList.addTask(new Event(description,
+                        convertStringToDateTime(from),
+                        convertStringToDateTime(to)));
             }
 
-            default ->
-                    throw new InvalidCommandException(command);
+            Storage.addTaskToSavedTasks(Casper.FILE_PATH, taskList.getTaskSaveString(taskList.size() - 1));
+            return Stringifier.getAddedTaskMessage(taskList.getTaskString(taskList.size() - 1), taskList.size());
+        }
+
+        default ->
+                throw new InvalidCommandException(command);
         }
     }
 }
